3b920896afa30d93c1b569689c65a80a08125447
Patrick <87815239+ptrckbp@users.noreply.github.com>
2024-04-26 14:41:20 -0400

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+
+import qs from 'qs'
+import { Integration, secrets } from '.botpress'
+
+const integration = new Integration({
+  handler: async ({ req, client, ctx }) => {
+    if (ctx.configuration.secret && req.headers['x-bp-secret'] !== ctx.configuration.secret) {
+      throw new Error('Invalid secret')
+    }
+
+    const method = req.method.toUpperCase()
+    if (!['POST', 'GET'].includes(method)) {
+      throw new Error('Invalid method')
+    }
+
+    const query = req.query ? qs.parse(req.query) : {}
+
+    let body = {}
+    try {
+      body = JSON.parse(req.body ?? '{}')
+    } catch (err) {}
+
+    await client.createEvent({
+      type: 'webhook:event',
+      payload: {
+        body,
+        query,
+        method,
+        path: req.path,
+      },
+    })
+  },
+  register: async () => {},
+  unregister: async () => {},
+  actions: {},
+  channels: {},
+})
+
+export default sentryHelpers.wrapIntegration(integration)
